72e768ca1626cbc2ec62cb9100fed94f0adb971c
VivianKoutroumani <73958693+VivianKoutroumani@users.noreply.github.com>
2020-11-30 00:45:17 +0200

    modified:   blog/__pycache__/models.cpython-38.pyc 	modified:   blog/__pycache__/urls.cpython-38.pyc 	modified:   blog/__pycache__/views.cpython-38.pyc modified:   blog/templates/blog/analysis.html 	modified:   blog/views.py 	modified:   db.sqlite3

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from .models import Post, Workout
+from datetime import datetime
 import matplotlib.pyplot as plt, mpld3
+import re
 
 
 def overview(request):
@@ -74,29 +76,69 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 def analysis(request):
     xAxisID= []
     yAxisID = []
+    time= []
+    time2 = []
+    time3=[]
+    speedid= []
     current_user=request.user
     queryset=Workout.objects.order_by('-date')
     for workout in queryset:
         if current_user==workout.athlete: 
             yAxisID.append(workout.distance)
             xAxisID.append(workout.date)
-    for i in range(0, len(yAxisID)):
+            time.append(workout.start_time)
+            time2.append(workout.end_time)
+        
+    for i in range(len(yAxisID)):
         yAxisID[i]=float(yAxisID[i])
-    for i in range(0, len(xAxisID)):
+        time[i]=str(time[i])
+        time[i] = re.sub('\+00:00', '', time[i])
+        time2[i]=str(time2[i])
+        time2[i] = re.sub('\+00:00', '', time2[i])   
+        time[i]=datetime.strptime(time[i],"%Y-%m-%d %H:%M:%S")
+        time2[i]=datetime.strptime(time2[i],"%Y-%m-%d %H:%M:%S")
+        
+    for i in range(len(time2)):
+        time3.append(time2[i]-time[i])
+
+    for i in range(len(time3)):
+        time3[i]=float(time3[i].total_seconds()/60)
+           
+    for i in range(len(xAxisID)):
         xAxisID[i]=str(xAxisID[i])
         xAxisID[i]=xAxisID[i].split(' ')[0]
+    i=1
+   # print("test2")
+    #if len(xAxisID)>1:
+    #   
+    while i!=len(xAxisID):
+        if xAxisID[i]==xAxisID[i-1]:
+            print("test")
+            yAxisID[i-1]=yAxisID[i]+yAxisID[i-1]
+            time3[i-1]=time3[i]+time3[i-1]
+            xAxisID.pop(i)
+            yAxisID.pop(i)
+            time3.pop(i)
+            i=i-1
+        i=i+1
     if len(yAxisID) > 7:
-        for x in range(7,len(yAxisID)):
+        for x in range(len(yAxisID)):
             if len(yAxisID) > 7:
                  xAxisID.pop(-1)
                  yAxisID.pop(-1)
+                 time3.pop(-1)
+    for i in range(len(time3)):
+        speedid.append((yAxisID[i]/time3[i]))
+    for i in range(len(speedid)):
+        speedid[i]=str(round(speedid[i], 2))
+
 
     return render(request, "blog/analysis.html", {
         'title': 'Workout Analysis',
         'labels': xAxisID,
         'data': yAxisID,
+        'speed' : speedid,
     })
-
   #queryset = Workout.objects.order_by('-date')[]
     #for Workout in queryset:
       #  labels.append(Workout.date)
